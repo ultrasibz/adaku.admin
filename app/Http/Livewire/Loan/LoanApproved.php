7c9aef07c708b5818903423791032c118dd7a5f8
Gilbert Sibajene <gilbertsibajene7@gmail.com>
2023-06-29 02:56:23 +0200

    Commiting

--- app/Http/Livewire/Loan/LoanApproved.php
+++ app/Http/Livewire/Loan/LoanApproved.php
@@ -5,9 +5,12 @@ namespace App\Http\Livewire\Loan;
 use App\Enums\LoanStatus;
 use App\Models\Loan;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class LoanApproved extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
     public function render()
     {
         $loans = Loan::with(['customer'])->where('status',LoanStatus::APPROVED)->paginate(10);
--- app/Http/Livewire/Loan/LoanIndex.php
+++ app/Http/Livewire/Loan/LoanIndex.php
@@ -5,12 +5,17 @@ namespace App\Http\Livewire\Loan;
 use App\Enums\LoanStatus;
 use App\Models\Loan;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class LoanIndex extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
     public function render()
     {
-        $loans = Loan::with(['customer'])->where('status',LoanStatus::PENDING)->paginate(10);
+        $loans = Loan::with(['customer'])->where('status', LoanStatus::PENDING)->paginate(10);
         return view('livewire.loan.loan-index', compact('loans'));
     }
 }
